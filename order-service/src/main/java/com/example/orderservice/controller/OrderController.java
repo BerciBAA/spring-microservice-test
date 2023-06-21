d3e194d3338d72e954f7f6d833bd9f2092afd638
BerciBAA <bercike222@gmail.com>
2023-06-21 15:42:34 +0200

    Spring boot porjet with discovery server and microservice arithecture.

---
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.service.OrderService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@AllArgsConstructor
+public class OrderController {
+
+    OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
